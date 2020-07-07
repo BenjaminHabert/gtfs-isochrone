efbce2c2142ae51b517bfa2735f76edf1996acf7
benjamin <benj.habert@gmail.com>
2020-07-07 19:31:44 +0200

    add main orchestrator function

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import click
 
 from gtfs_isochrone import prepare as _prepare
+from gtfs_isochrone.main import compute_isochrone
 
 
 @click.group()
@@ -14,7 +15,22 @@ def prepare(gtfs_folder):
     _prepare.prepare_data_in_gtfs_folder(gtfs_folder)
 
 
+@click.command()
+def demo():
+    import datetime
+
+    gtfs_folder = "data/orleans"
+    lat = 47.900792
+    lon = 1.903623
+    start_datetime = datetime.datetime(2020, 7, 2, 14)
+    max_duration_seconds = 20 * 60
+    print(
+        compute_isochrone(gtfs_folder, lat, lon, start_datetime, max_duration_seconds)
+    )
+
+
 cli.add_command(prepare)
+cli.add_command(demo)
 
 
 if __name__ == "__main__":
